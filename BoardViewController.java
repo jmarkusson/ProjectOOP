434b506368908a83634fee0460c5ef6d738aad18
ValleBisander <valdemar.bisander@gmail.com>
2023-11-09 10:19:16 +0100

    Controllers

--- BoardViewController.java
+++ BoardViewController.java
@@ -1,9 +1,18 @@
-public class BoardViewController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BoardViewController implements ActionListener {
     
     private RiskModel model;
     private BoardView view;
 
     protected BoardViewController(RiskModel model, BoardView view){
+        this.model = model;
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
         
     }
 
