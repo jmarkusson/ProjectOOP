4244f37e05ece0cf6996bda38f94638a0cf9a98b
AlfPett <alfredp.2000@gmail.com>
2023-12-06 14:17:12 +0100

    Jätte

---
+++ risk/src/main/java/group6/Main.java
@@ -0,0 +1,15 @@
+package group6;
+
+import group6.Controller.MainMenuController;
+import group6.Model.RiskModel;
+import group6.View.AttackView;
+import group6.View.MainMenuView;
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenuView view = new MainMenuView();
+        RiskModel model = new RiskModel();
+
+        MainMenuController controller = new MainMenuController(model, view);
+    }
+}
